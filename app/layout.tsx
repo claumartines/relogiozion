2d041b2a5533bcb854f80c42e09523f3de61a9e6
dnms.tech <tech.dnms@gmail.com>
2025-10-24 09:32:42 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Relógio de Oração e Jejum Corporativo",
+  description: "Relógio de oração e jejum corporativo - Zion Lisboa",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
